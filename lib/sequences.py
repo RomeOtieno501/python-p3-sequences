c9bd223ebaba8ecb92e3bda525d4e97dafcad4b8
Rome Ojuro Otieno <otienorome2@gmail.com>
2024-11-27 10:39:48 +0300

    Final commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fibonacci_sequence = [0, 1]
+        while len(fibonacci_sequence) < length:
+            next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_value)
+        print(fibonacci_sequence)
+
+# Example usage:
+print_fibonacci(9)
+# Output: [0, 1, 1, 2, 3, 5, 8, 13, 21]
